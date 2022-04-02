e2d88c9e8ec988b7961ef804f3040dbd125fed34
Obarskaia Tatiana <obarskaia.tatiana@gmail.com>
2022-04-02 09:47:35 +0300

    no more error output

--- task2/run.sh
+++ task2/run.sh
@@ -5,7 +5,7 @@ directory=$(basename $directory_path)
 ftype=.$2
 dir_name=$3
 archive_name=$4
-all_dirs=$(find $directory -name "*$ftype")
+all_dirs=$(find $directory -name "*$ftype" 2>/dev/null)
 mkdir $dir_name
 cp --parent $all_dirs $dir_name/ >/dev/null 2>/dev/null
 tar -cf $archive_name $dir_name >/dev/null 2>/dev/null
